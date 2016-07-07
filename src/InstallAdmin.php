3328ed3b03bfa3c1c9af5a7b93df5892a20b8a68
Benoît Ngô <ngobenoit@gmail.com>
2016-07-07 13:59:21 +0200

    Removing installer from the package

--- src/InstallAdmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Mouf\MoufManager;
-
-// Let's declare the contoller
-MoufManager::getMoufManager()->declareComponent('userentityinstaller', 'Security\\Daos\\Doctrine\\UserEntityInstallerController', true);
-// Let's bind the 'template' property of the controller to the 'installTemplate' instance
-MoufManager::getMoufManager()->bindComponents('userentityinstaller', 'template', 'moufInstallTemplate');
-MoufManager::getMoufManager()->bindComponents('userentityinstaller', 'contentBlock', 'block.content');
-?>
--- src/User.php.tpl
+++
@@ -1,7 +0,0 @@
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Table;
-
-/**
- * @Entity
- * @Table(name="security_doctrine_users")
- */
