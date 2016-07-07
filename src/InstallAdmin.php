b844ce48aa0f9f7d40fc30da787d4e59981ab3b0
Benoît Ngô <ngobenoit@gmail.com>
2016-07-07 11:29:10 +0200

    Majing installer namespace

--- src/InstallAdmin.php
+++ src/InstallAdmin.php
@@ -2,7 +2,7 @@
 use Mouf\MoufManager;
 
 // Let's declare the contoller
-MoufManager::getMoufManager()->declareComponent('userentityinstaller', 'Mouf\\Security\\Userdao\\UserEntityInstallerController', true);
+MoufManager::getMoufManager()->declareComponent('userentityinstaller', 'Security\\Daos\\Doctrine\\UserEntityInstallerController', true);
 // Let's bind the 'template' property of the controller to the 'installTemplate' instance
 MoufManager::getMoufManager()->bindComponents('userentityinstaller', 'template', 'moufInstallTemplate');
 MoufManager::getMoufManager()->bindComponents('userentityinstaller', 'contentBlock', 'block.content');
